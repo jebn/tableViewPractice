f471f39ea5d0f17b4019a7b4b346b85381bb3d36
Lalo <User@Lions-Mac.local>
2013-03-04 20:53:33 -0600

    Initial Commit

---
+++ tableViewPractice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tableViewPractice
+//
+//  Created by Lalo on 04/03/2013.
+//  Copyright (c) 2013 Lalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tableViewPractice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tableViewPractice
+//
+//  Created by Lalo on 04/03/2013.
+//  Copyright (c) 2013 Lalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
